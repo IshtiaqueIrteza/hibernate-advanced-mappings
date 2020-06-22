ac6bd5e199e1c5d1ac0cda476fdcc737ee5b992b
ishtiaque <irteza@synesisit.com.bd>
2020-06-22 22:12:33 +0600

    Initial commit

---
+++ src/com/hibernate/demo/HibernateDemoApplication.java
@@ -0,0 +1,11 @@
+package com.hibernate.demo;
+
+/**
+ * Created by Ishtiaque on 6/22/2020.
+ */
+public class HibernateDemoApplication {
+
+    public static void main(String args[]){
+        System.out.println("Hibernate Demo App");
+    }
+}
