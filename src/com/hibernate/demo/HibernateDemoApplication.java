a7b2bf7bd3d4382703c5f3e717638bb2a0c62084
ishtiaque <irteza@synesisit.com.bd>
2020-06-22 23:59:27 +0600

    Checked JDBC connection

--- src/com/hibernate/demo/HibernateDemoApplication.java
+++ src/com/hibernate/demo/HibernateDemoApplication.java
@@ -1,11 +1,24 @@
 package com.hibernate.demo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by Ishtiaque on 6/22/2020.
  */
 public class HibernateDemoApplication {
 
     public static void main(String args[]){
-        System.out.println("Hibernate Demo App");
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/test_db";
+        String user = "root";
+
+        try {
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
